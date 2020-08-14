a628e73702122814e0fdf2d5cee78a481d099da1
MonsieurGlock <chinnakitglock@gmail.com>
2020-08-14 13:13:31 +0700

    add array

--- order.cpp
+++ order.cpp
@@ -1,8 +1,15 @@
 #include <stdio.h>
 int main(){
-    int num[5] = {8,5,1,7,2},i,j,m;
-    for(i=0 ; i <= 5-2 ; i++ ){
-        for(j=0 ; j <= 5-i ; j++){
+    int i,j,m,n;
+    printf("How many number : ");
+    scanf("%d",&n);
+    int num[n];
+    for(i=0 ; i < n ; i++){
+        printf("Entter %d number :",i+1);
+        scanf("%d",&num[i]);
+    }
+    for(i=0 ; i <= n-2 ; i++ ){
+        for(j=0 ; j <= n-i ; j++){
             if(num[j]>num[j+1]){
                 m = num[j];
                 num[j] = num[j+1];
@@ -10,7 +17,7 @@ int main(){
                 }
              }    
     }
-    for(i=0 ; i <= 5-1 ; i++){
+    for(i=0 ; i <= n-1 ; i++){
         printf("%d ",num[i]);
     }
     return 0;
